48a8b7897cbce057df35d31858502b04fd82e645
oraigo-cloud <sagar.xbitlabs@gmail.com>
2025-10-15 09:46:23 +0530

    Auto commit on 2025-10-15 09:46:17

--- adventuresaathi/js/book-adv.js
+++ adventuresaathi/js/book-adv.js
@@ -1,6 +1,5 @@
 document.getElementById("bookingForm").addEventListener("submit", async function(e) {
   e.preventDefault();
-  alert('Thank you for contacting Adventure Saathi! We will reach out soon.');
 
   const form = e.target;
   const formData = new FormData();
@@ -13,6 +12,9 @@ document.getElementById("bookingForm").addEventListener("submit", async function
   formData.append("Date", form.date.value);
   formData.append("Travellers", form.travellers.value);
   formData.append("Notes", form.notes.value);
+
+  alert('Thank you for contacting Adventure Saathi! We will reach out soon.');
+
 
   try {
     const response = await fetch("https://script.google.com/macros/s/AKfycbz15jkIZpAFfwDM5QVVCUKTx7b4xfNTrz2SGJPaE4e9pwKf8py73Hd6IeLoG3_mf02u/exec", {
